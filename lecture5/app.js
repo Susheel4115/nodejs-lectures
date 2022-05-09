b926356444ee476323159cbf61843933c5362ade
Mr.Susheel Kumar <74233895+Susheel4115@users.noreply.github.com>
2022-05-09 17:47:23 +0530

    Learning node change db url

--- lecture5/app.js
+++ lecture5/app.js
@@ -1,118 +1,125 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const bookRoutes = require('./routes/bookRoutes');
-const userRoutes = require('./routes/userRoutes');
-const { seed, deleteDb } = require('./seed');
-const user = require('./model/user');
-const { genHash, comparePassword, genToken, verifyToken } = require('./utils');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const bookRoutes = require("./routes/bookRoutes");
+const userRoutes = require("./routes/userRoutes");
+const { seed, deleteDb } = require("./seed");
+const user = require("./model/user");
+const { genHash, comparePassword, genToken, verifyToken } = require("./utils");
 
 async function connectDB() {
-    const url = 'mongodb+srv://admin:admin@cluster0.xjkyg.mongodb.net/library?retryWrites=true&w=majority';
-    return new Promise((res, rej) => {
-        mongoose.connect(url, async function(err) {
-            if (err) {
-                console.log(err);
-                rej(err);
-            }
-            await seed();
-            res('Connected to DB');
-            // deleteDb()
-        });
-    }) 
+  const url =
+    "mongodb+srv://susi4115:SToOEQLLxqhb27C6@cluster0.lic5r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  // "mongodb+srv://susi4115:SToOEQLLxqhb27C6#@cluster0.xjkyg.mongodb.net/library?retryWrites=true&w=majority";
+  return new Promise((res, rej) => {
+    mongoose.connect(url, async function (err) {
+      if (err) {
+        console.log(err);
+        rej(err);
+      }
+      await seed();
+      res("Connected to DB");
+      // deleteDb()
+    });
+  });
 }
 
 async function main() {
-    try {
-        await connectDB();
+  try {
+    await connectDB();
 
-        app.use(bodyParser.urlencoded({ extended: true }));
-        app.use(bodyParser.json());
-        
-        app.get('/', function(req, res) {
-            res.send({
-                message: 'Welcome to Library API Service!'
-            });
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    app.get("/", function (req, res) {
+      res.send({
+        message: "Welcome to Library API Service!",
+      });
+    });
+
+    app.post("/api/v1/signup", async function (req, res) {
+      try {
+        const { firstName, lastName, email, password } = req.body;
+        const userExists = await user.findOne({ email });
+        if (userExists) {
+          const error = new Error("User Already Registered!");
+          error.statusCode = 400;
+          throw error;
+        }
+        const passwordHash = await genHash(password);
+        await user.create({
+          firstName,
+          lastName,
+          email,
+          password: passwordHash,
         });
-        
-        app.post('/api/v1/signup', async function(req, res) {
-            try {
-                const { firstName, lastName, email, password } = req.body;
-                const userExists = await user.findOne({ email });
-                if (userExists) {
-                    const error = new Error('User Already Registered!');
-                    error.statusCode = 400;
-                    throw error;
-                }
-                const passwordHash = await genHash(password);
-                await user.create({ firstName, lastName, email, password: passwordHash });
-                res.send({ message: 'Signup Successful' });
-            } catch(e) {
-                console.log(e);
-                res.status(e.statusCode || 500).send({
-                    error: e.message
-                })
-            }
-        });
-        
-        app.post('/api/v1/login', async function(req, res) {
-            try {
-                const { email, password } = req.body;
-                const userExists = await user.findOne({ email });
-                if (!userExists) {
-                    const error = new Error('User Not Found!');
-                    error.statusCode = 404;
-                    throw error;
-                }
-                const match = await comparePassword(password, userExists.password);
-                if (!match) {
-                    const error = new Error('Invalid Credentials!');
-                    error.statusCode = 400;
-                    throw error;
-                }
-                res.send({ token: genToken(userExists) });
-            } catch (e) {
-                console.log(e);
-                res.status(e.statusCode || 500).send({
-                    error: e.message
-                })
-            }
-        })
-        
-        app.use('/api/v1/*', async function(req, res, next) {
-            // console.log("I am in the middleware", req.headers);
-            try {
-                const authHeader = req.headers['authorization'];
-        
-                if (!authHeader) {
-                    return res.status(401).send({
-                        error: 'User not authorized!'
-                    });
-                } else {
-                    const token = authHeader.split('Bearer ')[1];
-                    const decoded = await verifyToken(token);
-                    req.userEmail = decoded.email;
-                    req.userType = decoded.userType;
-                }
-                next();
-            } catch(e) {
-                console.log(e);
-                return res.status(400).send({
-                    error: e.message
-                })
-            }
-        })
-        
-        app.use('/api/v1/books', bookRoutes);
-        app.use('/api/v1/users', userRoutes);
-        
-        app.listen(3000, function() {
-            console.log('Server started on Port 3000');
-        });
-    } catch(e) {
+        res.send({ message: "Signup Successful" });
+      } catch (e) {
         console.log(e);
-    }
+        res.status(e.statusCode || 500).send({
+          error: e.message,
+        });
+      }
+    });
+
+    app.post("/api/v1/login", async function (req, res) {
+      try {
+        const { email, password } = req.body;
+        const userExists = await user.findOne({ email });
+        if (!userExists) {
+          const error = new Error("User Not Found!");
+          error.statusCode = 404;
+          throw error;
+        }
+        const match = await comparePassword(password, userExists.password);
+        if (!match) {
+          const error = new Error("Invalid Credentials!");
+          error.statusCode = 400;
+          throw error;
+        }
+        res.send({ token: genToken(userExists) });
+      } catch (e) {
+        console.log(e);
+        res.status(e.statusCode || 500).send({
+          error: e.message,
+        });
+      }
+    });
+
+    app.use("/api/v1/*", async function (req, res, next) {
+      // console.log("I am in the middleware", req.headers);
+      try {
+        const authHeader = req.headers["authorization"];
+
+        if (!authHeader) {
+          return res.status(401).send({
+            error: "User not authorized!",
+          });
+        } else {
+          const token = authHeader.split("Bearer ")[1];
+          const decoded = await verifyToken(token);
+          req.userEmail = decoded.email;
+          req.userType = decoded.userType;
+        }
+        next();
+      } catch (e) {
+        console.log(e);
+        return res.status(400).send({
+          error: e.message,
+        });
+      }
+    });
+
+    app.use("/api/v1/books", bookRoutes);
+    app.use("/api/v1/users", userRoutes);
+
+    app.listen(3000, function () {
+      console.log("Server started on Port 3000");
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-main()
+main();
