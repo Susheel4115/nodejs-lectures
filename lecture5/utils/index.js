e42875bca57adda9d65a68905fbf63915d64b255
Mr.Susheel Kumar <74233895+Susheel4115@users.noreply.github.com>
2022-05-10 19:01:12 +0530

    Update index.js

--- lecture5/utils/index.js
+++ lecture5/utils/index.js
@@ -1,44 +1,48 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const SECRET = process.env.SECRET;
+const SECRET = process.env.SECRET || "xyz";
 
 async function genHash(password) {
-    return new Promise((res, rej) => {
-        bcrypt.hash(password, 10, function(err, hash) {
-            // Store hash in your password DB.
-            if (err) {
-                rej(err);
-            } else {
-                res(hash);
-            }
-        });
-    })
-};
+  return new Promise((res, rej) => {
+    bcrypt.hash(password, 10, function (err, hash) {
+      // Store hash in your password DB.
+      if (err) {
+        rej(err);
+      } else {
+        res(hash);
+      }
+    });
+  });
+}
 
 async function comparePassword(password, hash) {
-    const match = await bcrypt.compare(password, hash);
-    return match;
+  const match = await bcrypt.compare(password, hash);
+  return match;
 }
 
 function genToken(user) {
-    const token = jwt.sign({
-        email: user.email,
-        userType: user.userType
-    }, SECRET, { expiresIn: '1h' });
-    return token;
+  const token = jwt.sign(
+    {
+      email: user.email,
+      userType: user.userType,
+    },
+    SECRET,
+    { expiresIn: "1h" }
+  );
+  return token;
 }
 
 function verifyToken(token) {
-    return new Promise((res, rej) => {
-        jwt.verify(token, SECRET, function(err, decoded) {
-            if (err) {
-                rej(err);
-            } else {
-                res(decoded);
-            }
-        });
-    })
+  return new Promise((res, rej) => {
+    jwt.verify(token, SECRET, function (err, decoded) {
+      if (err) {
+        rej(err);
+      } else {
+        res(decoded);
+      }
+    });
+  });
 }
 
 module.exports = { genHash, comparePassword, genToken, verifyToken };
